ec4ce9abfa6bb0e5a7467b557fbf4457f646f338
Herbert de Sousa Santoss <117468625+SilveStredecendent@users.noreply.github.com>
2025-08-13 22:41:07 +0000

    segundo commit

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -1,7 +1,7 @@
 package app;
 
-public class PrimeiraClasse (
-    public static void main(String[] args){
-        System.out.prinln("Olá Gradle!!!");
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá Gradle!!!");
     }
-)
+}
