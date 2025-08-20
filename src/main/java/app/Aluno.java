4b0a6ed3e9976b65d851a7f38b6db5b57fa06146
Herbert de Sousa Santoss <117468625+SilveStredecendent@users.noreply.github.com>
2025-08-20 01:41:59 +0000

    Aula 19/08/2025

---
+++ src/main/java/app/Aluno.java
@@ -0,0 +1,11 @@
+package app;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class Aluno {
+    private long id;
+    private String nome;
+    private int idade;
+}
--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -3,5 +3,7 @@ package app;
 public class PrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Olá Gradle!!!");
+
+        Aluno al = new aluno();
     }
 }
